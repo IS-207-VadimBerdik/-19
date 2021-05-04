f1c591296646abb086de2d0ae5444b4a9efcdc75
VadimBerdik <is.207.vadimberdik@gmail.com>
2021-05-04 17:56:01 +0300

    готово

--- Program.cs
+++ Program.cs
@@ -7,12 +7,74 @@ namespace Практическая__19
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\nШуточное приложения для просмотра состояний системы\n\t(нажмите q/й и enter,чтобы выйти)");
 
+            Thread th_tempetatur = new Thread(PC_temperatur);
+            th_tempetatur.IsBackground = true;
+            th_tempetatur.Start();
+
+            Thread th_cpu = new Thread(PC_CPU);
+            th_cpu.IsBackground = true;
+            th_cpu.Start();
+
+            Thread th_hdd = new Thread(PC_HDD);
+            th_hdd.IsBackground = true;
+            th_hdd.Start();
+
+
+            Thread th_time = new Thread(PC_time);
+            th_time.IsBackground = true;
+            th_time.Start();
+
+            string key = null;
+            while (true)
+            {
+                key = Console.ReadLine();
+
+                if (key == "q" || key == "й")
+                {
+                    break;
+                }
+            };
         }
 
-        int PC_time()
+        static void PC_time()
         {
-            return Random
+            while (true)
+            {
+                Console.WriteLine($"- Время: {DateTime.Now}");
+                Console.WriteLine("\n\n");
+                Thread.Sleep(2000);
+            }
+        }
+
+        static void PC_temperatur()
+        {
+            while (true)
+            {
+                var temp = new Random();
+                Console.WriteLine($"- Температура: {temp.Next(30, 100)}C");
+                Thread.Sleep(2000);
+            }
+        }
+
+        static void PC_CPU()
+        {
+            while (true)
+            {
+                var temp = new Random();
+                Console.WriteLine($"- Нагрузка на процессор: {temp.Next(0, 100)}%");
+                Thread.Sleep(2000);
+            }
+        }
+        static void PC_HDD()
+        {
+            while (true)
+            {
+                var temp = new Random();
+                Console.WriteLine($"- Обороты диска: {temp.Next(2000, 5000)}");
+                Thread.Sleep(2000);
+            }
         }
     }
 }
