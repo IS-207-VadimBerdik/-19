13944548a968f4c0f97b01ae581c54753bc2fd6d
VadimBerdik <is.207.vadimberdik@gmail.com>
2021-05-04 17:50:14 +0300

    first version

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Практическая__19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        int PC_time()
+        {
+            return Random
+        }
+    }
+}
